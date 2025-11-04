54bad50530711179927b5b51ec7874dcf2d889c4
KateyKwesi <kateykwesi001@gmail.com>
2025-11-04 18:49:35 +0000

    fix image links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
     <footer>
-      <img src="src\assets\Facebook Icon.svg" alt="facebook icon" />
-      <img src="src\assets\Instagram Icon.svg" alt="instagram icon" />
-      <img src="src\assets\Twitter Icon.svg" alt="twitter icon" />
-      <img src="src\assets\GitHub Icon.svg" alt="github" />
+      <img src="./src/assets/Facebook Icon.svg" alt="facebook icon" />
+      <img src="./src/assets/Instagram Icon.svg" alt="instagram icon" />
+      <img src="./src/assets/Twitter Icon.svg" alt="twitter icon" />
+      <img src="./src/assets/GitHub Icon.svg" alt="github" />
     </footer>
   );
 };
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,17 +1,17 @@
 export default function Info() {
   return (
     <div className="info-container">
-      <img src="src\assets\bald-man.svg" alt="profile image" />
+      <img src="./src/assets/bald-man.svg" alt="profile image" />
       <h1>Katey Kwesi</h1>
       <p>Frontend Developer</p>
       <small>kateykwesi.com</small>
       <div className="socials">
         <div className="email">
-          <img src="src\assets\Mail.svg" alt="email icon" />
+          <img src="./src/assets/Mail.svg" alt="email icon" />
           <p>Email</p>
         </div>
         <div className="linkedin">
-          <img src="src\assets\linkedin.svg" alt="Linkedin icon" />
+          <img src="./src/assets/linkedin.svg" alt="Linkedin icon" />
           <p>Linkedin</p>
         </div>
       </div>
